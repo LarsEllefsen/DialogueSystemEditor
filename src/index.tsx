8c8bad514584c3c2925c667a7044434754e47584
Lars Moe Ellefsen <larsmoeellefsen@gmail.com>
2021-09-19 13:57:52 +0200

    Set up basic project

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { EditorView } from './views/EditorView';
+
+ReactDOM.render(
+  <EditorView />,
+  document.getElementById('app'),
+);
---
+++ src/views/EditorView.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const EditorView = () => <h1 className="text-2xl">Editor view</h1>;
